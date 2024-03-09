e8f1f0e526c2ae3ac758e329ab187adc48c30572
prekkk <121798993+prekkk@users.noreply.github.com>
2024-03-09 13:33:34 +0545

    started admin panel

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AccountController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\JobsController;
+use App\Http\Controllers\admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,8 +26,22 @@ Route::get('/jobs/detail/{id',[JobsController::class,'detail'])->name('jobDetail
 Route::post('/apply-job',[JobsController::class,'applyJob'])->name('applyJob');
 Route::post('/save-job',[JobsController::class,'saveJob'])->name('saveJob');
 
+Route::group(['prefix' => 'admin','middleware' => 'checkRole'], function(){
+    Route::get('/dashboard',[DashboardController::class,'index'])->name('admin.dashboard');
+    // Route::get('/users',[UserController::class,'index'])->name('admin.users');
+    // Route::get('/users/{id}',[UserController::class,'edit'])->name('admin.users.edit');
+    // Route::put('/users/{id}',[UserController::class,'update'])->name('admin.users.update');
+    // Route::delete('/users',[UserController::class,'destroy'])->name('admin.users.destroy');
+    // Route::get('/jobs',[JobController::class,'index'])->name('admin.jobs');
+    // Route::get('/jobs/edit/{id}',[JobController::class,'edit'])->name('admin.jobs.edit');
+    // Route::put('/jobs/{id}',[JobController::class,'update'])->name('admin.jobs.update');
+    // Route::delete('/jobs',[JobController::class,'destroy'])->name('admin.jobs.destroy');
+    // Route::get('/job-applications',[JobApplicationController::class,'index'])->name('admin.jobApplications');
+    // Route::delete('/job-applications',[JobApplicationController::class,'destroy'])->name('admin.jobApplications.destroy');
+});
 
-Route::group(['account'],function(){
+
+Route::group(['prefix' => 'account'], function(){
 
     // Guest Route
 Route::group(['middleware' => 'guest'],function(){
