fbf6f94096c2f9f46497ffab1147ddd989a6e89e
prekkk <121798993+prekkk@users.noreply.github.com>
2024-03-13 20:40:59 +0545

    completed admin panel

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AccountController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\JobsController;
 use App\Http\Controllers\admin\JobController;
+use App\Http\Controllers\admin\JobApplicationController;
 use App\Http\Controllers\admin\UserController;
 use App\Http\Controllers\admin\DashboardController;
 use Illuminate\Support\Facades\Route;
@@ -38,8 +39,8 @@ Route::group(['prefix' => 'admin','middleware' => 'checkRole'], function(){
     Route::get('/jobs/edit/{id}',[JobController::class,'edit'])->name('admin.jobs.edit');
     Route::put('/jobs/{id}',[JobController::class,'update'])->name('admin.jobs.update');
     Route::delete('/jobs',[JobController::class,'destroy'])->name('admin.jobs.destroy');
-    // Route::get('/job-applications',[JobApplicationController::class,'index'])->name('admin.jobApplications');
-    // Route::delete('/job-applications',[JobApplicationController::class,'destroy'])->name('admin.jobApplications.destroy');
+    Route::get('/job-applications',[JobApplicationController::class,'index'])->name('admin.jobApplications');
+    Route::delete('/job-applications',[JobApplicationController::class,'destroy'])->name('admin.jobApplications.destroy');
 });
 
 
